3577662f25e3e79306cc4e4fb1aa8fa0a2990a32
Paola Becerra <paonata_marquez@outlook.com>
2024-09-19 08:52:16 -0500

    add graphql service to db service to return products

--- src/graphql/product.resolvers.js
+++ src/graphql/product.resolvers.js
@@ -1,16 +1,17 @@
-const product = (_, args) => {
-  return {
-    id: args.id,
-    name: "Product 1",
-    price: 100.0,
-    description: "desc",
-    image: "https://image.asas",
-    createdAt: new Date().toISOString(),
-  };
+const ProductService = require("./../services/product.service");
+const service = new ProductService();
+
+const product = async (_, args) => {
+  const id = args.id;
+  const product = await service.findOne(id);
+
+  return product;
 };
 
-const allProducts = (_, args) => {
-  return [];
+const allProducts = async (_, args) => {
+  const products = await service.find({});
+
+  return products;
 };
 
 module.exports = { product, allProducts };
