26c6813b2c6283c9ecc17f160d82a4763bbe8bc8
Paola Becerra <paonata_marquez@outlook.com>
2024-09-23 16:03:25 -0500

    add validation to category creations

--- src/graphql/category.resolvers.js
+++ src/graphql/category.resolvers.js
@@ -1,10 +1,13 @@
 const CategoryService = require("./../services/category.service");
-
+const boom = require("@hapi/boom");
 const service = new CategoryService();
 
-const addCategory = (_, args) => {
+const addCategory = async (_, args, context) => {
   const newCategoryInfo = args.dto;
-  
+  const { user } = await context.authenticate("jwt", { session: false });
+  if (!user) {
+    throw boom.unauthorized("jwt is not valid");
+  }
   return service.create(newCategoryInfo);
 };
 
