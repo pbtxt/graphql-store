59c9b2c024aa40892a90e2640325cc79e03a6610
Paola Becerra <paonata_marquez@outlook.com>
2024-09-24 11:59:19 -0500

    add role validation before creating a category

--- src/graphql/category.resolvers.js
+++ src/graphql/category.resolvers.js
@@ -1,13 +1,18 @@
 const CategoryService = require("./../services/category.service");
 const boom = require("@hapi/boom");
 const service = new CategoryService();
+const { checkRolesGql } = require("./../utils/checkRolesGql");
 
 const addCategory = async (_, args, context) => {
   const newCategoryInfo = args.dto;
   const { user } = await context.authenticate("jwt", { session: false });
+
   if (!user) {
     throw boom.unauthorized("jwt is not valid");
   }
+
+  checkRolesGql(user, "admin");
+
   return service.create(newCategoryInfo);
 };
 
