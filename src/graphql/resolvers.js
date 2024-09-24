e9d2b14ea5b9c607501c4b2e1a222f82fbd9eab1
Paola Becerra <paonata_marquez@outlook.com>
2024-09-24 13:15:12 -0500

    add new type/scalar to validate category name rules

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -7,6 +7,12 @@ const {
 } = require("./product.resolvers");
 const { login } = require("./auth.resolvers");
 const { addCategory } = require("./category.resolvers");
+const { RegularExpression } = require("graphql-scalars");
+
+const CategoryName = new RegularExpression(
+  "CategoryNameType",
+  /^[a-zA-Z0-9]{3,8}$/
+);
 
 const resolvers = {
   Query: {
@@ -20,6 +26,7 @@ const resolvers = {
     login,
     addCategory,
   },
+  CategoryName,
 };
 
 module.exports = { resolvers };
